dc6e3905d027d886070e9499f555a133bd4238a2
Erwan CALVEZ <ecalvez@enib.fr>
2022-12-28 19:37:47 +0100

    update score display

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,26 +1,52 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, ecs::schedule::ShouldRun};
 use crate::{ utils};
 use super::{GameState};
 
 mod tile;
 mod score;
+
+#[derive(Clone, Eq, PartialEq, Debug, Hash)]
+pub enum TurnState {
+    Player,
+    Cpu,
+}
 pub struct GamePlugin;
 
 impl Plugin for GamePlugin{
     fn build(&self, app: &mut App) {
     app
+    .add_event::<score::NextTurnEvent>()
+    .init_resource::<score::Score>()
+    .add_state(TurnState::Cpu)
     .add_system_set(SystemSet::on_enter(GameState::Game)
-    .with_system(score::spawn_score)
-    .with_system(tile::spawn_tiles)
-)
-.add_system_set(SystemSet::on_update(GameState::Game)
-    .with_system(tile::toggle_tile)
-    .with_system(tile::reset_tile_color))
-    .add_system_set(
-        SystemSet::on_exit(GameState::Splash)
-            .with_system(utils::despawn_screen::<OnGameScreen>),
+        .with_system(tile::spawn_tiles)
+        .with_system(score::spawn_score)
+    )
+    .add_system_set(SystemSet::on_enter(TurnState::Cpu)
+        .with_system(score::move_to_next_turn)
+        .with_system(score::next_turn)
+        .with_system(score::update_score)
+        .with_run_criteria(run_if_new_game))
+    
+    .add_system_set(SystemSet::on_update(GameState::Game)
+        .with_system(tile::toggle_tile)
+        .with_system(tile::reset_tile_color)
+    )
+    .add_system_set(SystemSet::on_exit(GameState::Splash)
+        .with_system(utils::despawn_screen::<OnGameScreen>),
     );
     
+    }
+}
+
+fn run_if_new_game(
+    app_state: Res<State<GameState>>
+) -> ShouldRun
+{
+    if *app_state.current() == GameState::Game {
+        ShouldRun::Yes
+    } else {
+        ShouldRun::No
     }
 }
 
--- src/game/score/mod.rs
+++ src/game/score/mod.rs
@@ -2,8 +2,29 @@ use bevy::prelude::*;
 
 use crate::utils;
 
+use super::TurnState;
+
+pub struct ScorePlugin;
+
+impl Plugin for ScorePlugin {
+    fn build(&self, app: &mut App) {
+        app
+        .init_resource::<Score>()
+        .add_event::<NextTurnEvent>()
+        .add_startup_system(spawn_score)
+        .add_system(next_turn);
+    }
+}
+
+pub struct NextTurnEvent(TurnState);
+
+#[derive(Default,Resource)]
+pub struct Score {
+    pub score: u32,
+}
+
 #[derive(Component)]
-pub struct Score;
+pub struct ScoreDisplay;
 
 pub fn spawn_score(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands
@@ -16,7 +37,6 @@ pub fn spawn_score(mut commands: Commands, asset_server: Res<AssetServer>) {
             },
             ..Default::default()
         })
-        .insert(Score)
         .with_children(|parent| {
             parent.spawn(TextBundle {
                 text: Text::from_section("Score",TextStyle {
@@ -26,8 +46,33 @@ pub fn spawn_score(mut commands: Commands, asset_server: Res<AssetServer>) {
                     ..Default::default()
                 }),                
                 ..default()
-            });
+            })
+            .insert(ScoreDisplay);
             let gear_button = utils::build_icons(asset_server, "gear".to_string());
             parent.spawn(gear_button);
             });
         }
+
+pub fn move_to_next_turn(keys: Res<Input<KeyCode>>,mut turn_state: ResMut<State<TurnState>>, mut ev_next_turn: EventWriter<NextTurnEvent>) {
+    if keys.just_pressed(KeyCode::Space) {
+        println!("Space key pressed!");
+        let next_state = match turn_state.current() {
+            TurnState::Cpu => TurnState::Player,
+            TurnState::Player =>TurnState::Cpu
+        };
+        ev_next_turn.send(NextTurnEvent(next_state.clone()));
+        turn_state.set(next_state).unwrap();
+    }
+}
+
+pub fn next_turn(mut ev_next_turn: EventReader<NextTurnEvent>, mut score: ResMut<Score>) {
+    for _ in ev_next_turn.iter() {
+        score.score+= 1;
+    }
+}
+
+pub fn update_score(score: Res<Score>,mut text_query: Query<&mut Text,With<ScoreDisplay>>) {
+    for mut text in text_query.iter_mut() {
+        text.sections[0].value = format!("{}{}","Score: ",score.score);
+    }
+}
